fb11c7d7fd9a0bd74f06717ffca1e46ab08d9c2a
MateiMadalina <116349352+MateiMadalina@users.noreply.github.com>
2023-10-17 14:45:00 +0300

    Update WhatsANameIn

--- src/main/java/WhatsANameIn.java
+++ src/main/java/WhatsANameIn.java
@@ -3,16 +3,15 @@ import java.util.List;
 
 public class WhatsANameIn {
     public static void main(String[] args) {
-        System.out.println(nameInStr("v", "Madalina"));
+        System.out.println(nameInStr("Across the rivers", "chris"));
     }
     public static boolean nameInStr(String str, String name){
-        List<Character> letters = new ArrayList<Character>();
-        for (Character c : str.toCharArray()) {
-            for(Character ch : name.toCharArray()){
-                if(c.equals(ch)) letters.add(ch);
-            }
+        int i = 0;
+
+        for (char s: str.toCharArray()) {
+            if (s == name.charAt(i)) i++;
         }
-        System.out.println(letters);
-        return true;
+
+        return i == name.length();
     }
 }
