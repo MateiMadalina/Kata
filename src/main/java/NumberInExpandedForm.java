7fa8e4971718edc77be86036d5957c3119172d61
MateiMadalina <116349352+MateiMadalina@users.noreply.github.com>
2023-12-18 14:20:48 +0200

    new solution for explanded number with stream

--- src/main/java/NumberInExpandedForm.java
+++ src/main/java/NumberInExpandedForm.java
@@ -1,16 +1,30 @@
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class NumberInExpandedForm {
     public static void main(String[] args) {
         System.out.println(expandedForm(3507));
     }
 
     public static String expandedForm(int num) {
-        Integer number = Integer.valueOf(num);
-        String result = "";
-        for (int i = 0; i < number.toString().length(); i++) {
-            if (number.toString().charAt(i) != '0') {
-                result += (int) (Integer.parseInt(String.valueOf(number.toString().charAt(i))) * Math.pow(10, number.toString().length() - i - 1)) + " + ";
-            }
-        }
-        return result.substring(0, result.length() - 3);
+//        StringBuilder result = new StringBuilder();
+//        for (int i = 0; i < Integer.toString(num).length(); i++) {
+//            if (Integer.toString(num).charAt(i) != '0') {
+//                result.append((int) (Integer.parseInt(String.valueOf(Integer.toString(num).charAt(i))) * Math.pow(10, Integer.toString(num).length() - i - 1))).append(" + ");
+//            }
+//        }
+//        return result.substring(0, result.length() - 3);
+
+        String numString = Integer.toString(num);
+        int length = numString.length();
+
+        return IntStream.range(0, length)
+                .mapToObj(i -> numString.charAt(i) != '0'
+                        ? String.valueOf(numString.charAt(i) + "0".repeat(length - i - 1))
+                        : null)
+                .filter(Objects::nonNull)
+                .collect(Collectors.joining(" + "));
+
     }
 }
