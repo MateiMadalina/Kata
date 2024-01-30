8c2f534dc7fa57a3ad23079acb7923b46da6f2f9
MateiMadalina <116349352+MateiMadalina@users.noreply.github.com>
2024-01-30 13:40:51 +0200

    Rectangle into squares

---
+++ src/main/java/RectangleIntoSquares.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class RectangleIntoSquares {
+    public static void main(String[] args) {
+        System.out.println(sqInRect(5, 3));
+    }
+    public static List<Integer> sqInRect(int lng, int wdth) {
+        List<Integer> sizesOfTheSquares = new ArrayList<>();
+        while(lng != wdth){
+            if(lng > wdth){
+                lng = lng - wdth;
+                sizesOfTheSquares.add(wdth);
+            }else{
+                wdth = wdth- lng;
+                sizesOfTheSquares.add(lng);
+            }
+        }
+        if(sizesOfTheSquares.size()>0){
+            sizesOfTheSquares.add(lng);
+            return sizesOfTheSquares;
+        }
+        return null;
+    }
+}
